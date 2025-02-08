2efdda50657e6bac959513e2529423f637b9230a
Lucas de Lima Bica <delimabica@gmail.com>
2025-02-08 16:48:56 +0000

    Added a column with a minus button right under the counter

--- src/main.py
+++ src/main.py
@@ -8,15 +8,38 @@ def main(page: ft.Page):
         counter.data += 1
         counter.value = str(counter.data)
         counter.update()
+        
+    def decrease_click(e):
+        counter.data -= 1
+        counter.value = str(counter.data)
+        counter.update()
 
-    page.floating_action_button = ft.FloatingActionButton(
+    page.floating_action_button = ft.FloatingActionButton( # Adding to the 'page obj' the sum button 
         icon=ft.Icons.ADD, on_click=increment_click
     )
+    
+    #page.floating_action_button_2 = ft.FloatingActionButton(
+    #    icon=ft.Icons.AIRPLANEMODE_ON, on_click=decrease_click
+    #)
+    
+    minus_button = ft.ElevatedButton(
+        text="Minus",
+        on_click=decrease_click,
+        icon=ft.Icons.MINIMIZE
+    )
+    
     page.add(
         ft.SafeArea(
-            ft.Container(
-                counter,
-                alignment=ft.alignment.center,
+            ft.Column(
+                [
+                    ft.Container(
+                        counter,
+                        alignment=ft.alignment.center,
+                    ),
+                    minus_button,
+                ],
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                expand=True,
             ),
             expand=True,
         )
