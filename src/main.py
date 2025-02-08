96be4241430e418fdafd11015cfaea6bbe22caba
Lucas de Lima Bica <delimabica@gmail.com>
2025-02-08 17:50:40 +0000

    Creating tasks and embedded checkbox to it with PAGE.ADD

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.title = "My First App"
+    def add_button_clicked(e):
+        page.add(
+            ft.Checkbox(label=new_task.value)
+        )
+        new_task.value = ""
+        page.update()
+    
+    page.title = "To-Doz" # Defining the app's UI
     page.add(
         ft.Text(value="Hello, World!"),
     )
+        
+    new_task = ft.TextField(hint_text="What's need to be done?") # The datafield to insert the tasks
+    page.add(new_task, ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=add_button_clicked)) # Calling the 'page.add' for each new entry in 'new_task' field above
 
 ft.app(main)
