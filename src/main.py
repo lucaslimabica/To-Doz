e3b1b4b9cc4d5409a58ac040a563ac59aab813f6
Lucas de Lima Bica <delimabica@gmail.com>
2025-02-09 20:08:23 +0000

    feat: editing tasks is now possible

--- src/main.py
+++ src/main.py
@@ -13,7 +13,11 @@ class ToDozApp(ft.Column): # ihnerance
         self.insert_widget = ft.Row( # The 'input line' 
             controls=[
                 self.new_task,
-                ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=self.add_task), # The add button right by side of the textbar
+                ft.FloatingActionButton(
+                    icon=ft.Icons.ADD,
+                    on_click=self.add_task,
+                    tooltip="Add a task!"
+                ), # The add button right by side of the textbar
             ],
             expand=True,
         )
@@ -41,7 +45,7 @@ class Task(ft.Column):
         self.task_name = task_name
         self.task_delete = task_delete # The Apps func to delete a task :)
         self.task_header = ft.Checkbox(value=False, label=self.task_name)
-        self.edit_name = ft.TextField(expand=1)
+        self.edit_name = ft.TextField(expand=True, hint_text="New Task's Name")
         
         self.header_view = ft.Row(
             alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
@@ -66,13 +70,36 @@ class Task(ft.Column):
             ]
         )
         
-        self.controls = [self.header_view]
+        self.edit_view = ft.Row(
+            visible=False,
+            alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                self.edit_name,
+                ft.IconButton(
+                    icon=ft.Icons.DONE_ROUNDED,
+                    icon_color=ft.Colors.GREEN,
+                    tooltip="Update Task",
+                    on_click=self.save_clicked,
+                )
+            ],
+        )
+        
+        self.controls = [self.header_view, self.edit_view]
         
     def delete_clicked(self, e): # When clicked calls the func of the app
         self.task_delete(self)
     
     def edit_clicked(self, e):
-        pass
+        self.edit_name.value = self.task_header.label
+        self.header_view.visible = False
+        self.edit_view.visible = True
+        self.update()
+    
+    def save_clicked(self, e):
+        self.task_header.label = self.edit_name.value
+        self.header_view.visible = True
+        self.edit_view.visible = False
+        self.update()
         
 def main(page: ft.Page):
     to_do_app = ToDozApp() # The instance
